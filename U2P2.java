8a43637be7e0944e86a6487c686bd01cb2f26506
kinjal-nasit-12 <kinjalnasit12@gmail.com>
2024-08-03 21:11:55 -0700

    Update U2P2.java

--- U2P2.java
+++ U2P2.java
@@ -1,16 +1,20 @@
 /* Write a program to create an array to store 5 integer values. Also
 initialize the array with 5 numbers and display the array Elements
 in reverse order. */
+import java.util.Scanner;
 class U2P2 {
 
     public static void main(String args[]) {
-        int a[] = {2, 4, 3, 6, 1};
-        for (int i = 0; i < a.length; i++) {
-            System.out.println("a[" + i + "]= " + a[i]);
+        int array[] = new int[5];
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter 5 integer values: ");
+        for (int i = 0; i < 5; i++) {
+            array[i]=scanner.hasNextInt() ? scanner.nextInt() : 0;
+            System.out.println("a [" + i + "]= " + array[i]);
         }
         System.out.println("Array in reverse order: ");
-        for (int i = a.length - 1; i >= 0; i--) {
-            System.out.println("a[" + i + "]= " + a[i]); 
+        for (int i = array.length - 1; i >= 0; i--) {
+            System.out.println("a [" + i + "]= " + array[i]); 
         }
     }
 }
